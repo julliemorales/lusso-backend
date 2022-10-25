386ae4f9d5804d93dbf04c9f8070c320713f5228
Jullie Angeli Morales <jullieangelimorales05@gmail.com>
2022-10-25 22:03:43 +0800

    Installed CORS

--- server.js
+++ server.js
@@ -1,19 +1,22 @@
-const express = require("express");
-const { errorHandler, notFound } = require("./middlewares/errorMiddleware");
-const connectDB = require("./config/db");
-const dotenv = require("dotenv").config();
+const express = require('express');
+const cors = require('cors');
+const { errorHandler, notFound } = require('./middlewares/errorMiddleware');
+const connectDB = require('./config/db');
+const dotenv = require('dotenv').config();
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+app.use(cors());
 
 connectDB();
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("API RUNNING");
+app.get('/', (req, res) => {
+  res.send('API RUNNING');
 });
-app.use("/api/products", require("./routes/productRoutes"));
-app.use("/api/users", require("./routes/userRoutes"));
-app.use("/api/orders", require("./routes/cartRoutes"));
+app.use('/api/products', require('./routes/productRoutes'));
+app.use('/api/users', require('./routes/userRoutes'));
+app.use('/api/orders', require('./routes/cartRoutes'));
 app.use(errorHandler);
 app.use(notFound);
 
